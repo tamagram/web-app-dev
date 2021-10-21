c4e51b20cdfe08896e059d964ed63f3f6f8f33ca
tamagram <tamagram64@gmail.com>
2021-10-21 23:13:43 +0900

    :white_check_mark: Add test.

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const { requestParse } = require("./index");
 
 describe("test requestParse", () => {
-  test("should return the request context", () => {
+  test("should return the get request context", () => {
     const data = `GET / HTTP/1.1
 Host: localhost:8080
 User-Agent: curl/7.68.0
@@ -13,6 +13,34 @@ Accept: */*`;
       "Host": "localhost:8080",
       "User-Agent": "curl/7.68.0",
       "Accept": "*/*",
+    };
+    const got = requestParse(data);
+    // console.log(got);
+    expect(got).toStrictEqual(want);
+  });
+
+  test("should return the post request context", () => {
+    const data = `POST / HTTP/1.1
+Host: localhost:8080
+User-Agent: curl/7.68.0
+Accept: */*
+Content-Length: 11
+Content-Type: application/x-www-form-urlencoded
+
+hand=グー`;
+    const want = {
+      "httpMethod": "POST",
+      "path": "/",
+      "protocol": "HTTP/1.1",
+      "Host": "localhost:8080",
+      "User-Agent": "curl/7.68.0",
+      "Accept": "*/*",
+      "Content-Length": "11",
+      "Content-Type": "application/x-www-form-urlencoded",
+
+      "params": {
+        "hand": "グー",
+      },
     };
     const got = requestParse(data);
     console.log(got);

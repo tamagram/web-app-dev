b6844089df30f45aec0b08e960072fd353b36b96
tamagram <tamagram64@gmail.com>
2021-10-21 22:32:23 +0900

    :ballot_box_with_check: Pass test.

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ const net = require("net");
 const statusLine = "HTTP/1.1 200 OK\r\n";
 const header = "Host: AppServer\r\n";
 
-const server = net.createServer((socket) => {
-  console.log("通信きたよ!");
+const requestParse = (data) => {
+  const requestHeaders = data.split("\n").map((requestHeader) => requestHeader);
+  let event = {};
+  [event["httpMethod"], event["path"], event["protocol"]] =
+    requestHeaders[0].split(" ");
+  requestHeaders.slice(1).forEach((header) => {
+    let [key, ...value] = header.split(":");
+    if (value === undefined) {
+      value = "";
+    } else {
+      value = value.join(":");
+    }
+    value = value.trim();
+    event[key] = value;
+  });
+  return event;
+};
 
+const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     console.log(data.toString());
 
@@ -19,3 +35,7 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(8080);
+
+module.exports = {
+  requestParse: requestParse,
+};

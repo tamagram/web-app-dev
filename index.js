4219e8a00349cb50b4beed70594a3e793cbaaccb
tamagram <tamagram64@gmail.com>
2021-10-21 23:13:58 +0900

    :ballot_box_with_check: Pass test.

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const statusLine = "HTTP/1.1 200 OK\r\n";
 const header = "Host: AppServer\r\n";
 
 const requestParse = (data) => {
-  const requestHeaders = data.split("\n").map((requestHeader) => requestHeader);
+  const [requestHeaders, requestParams] = data.split("\n\n");
+  const headers = requestHeaders.split("\n");
+  const params = requestParams ? requestParams.split("&") : [];
+
   let event = {};
   [event["httpMethod"], event["path"], event["protocol"]] =
-    requestHeaders[0].split(" ");
-  requestHeaders.slice(1).forEach((header) => {
+    headers[0].split(" ");
+
+  headers.slice(1).forEach((header) => {
     let [key, ...value] = header.split(":");
     if (value === undefined) {
       value = "";
@@ -18,13 +22,25 @@ const requestParse = (data) => {
     value = value.trim();
     event[key] = value;
   });
+
+  if (0 < params.length) {
+    event["params"] = {};
+    params.forEach((param) => {
+      let [key, value] = param.split("=");
+      event["params"][key] = value;
+    });
+  }
   return event;
 };
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     console.log(data.toString());
+    const event = requestParse(data.toString());
 
+    switch (event["path"]) {
+      case "/janken":
+    }
     let response = "";
 
     response = statusLine + header + "\r\n" + "HELLO WORLD!\r\n";
